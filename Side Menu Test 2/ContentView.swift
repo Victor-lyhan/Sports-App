3f3cba36f16142f4d8c5acfb311c1de27a829736
Victor Han <liyanghan1112@gmail.com>
2022-07-28 23:07:07 -0500

    Initial Commit

---
+++ Side Menu Test 2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Side Menu Test 2
+//
+//  Created by Victor Han on 2022/7/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Side Menu Test 2/Side_Menu_Test_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Side_Menu_Test_2App.swift
+//  Side Menu Test 2
+//
+//  Created by Victor Han on 2022/7/28.
+//
+
+import SwiftUI
+
+@main
+struct Side_Menu_Test_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
